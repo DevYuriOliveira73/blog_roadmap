99451c4a4834f2acfd1027715dc40c85ca4c35fe
DevYuriOliveira73 <oliveiracc@alu.ufc.br>
2026-07-15 14:43:02 -0300

    feat: remove post routes and associated file

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import userRoutes from "./user.route";
-import postRoutes from "./post.route";
 import createPostRoutes from "./createPost.route";
 
 
@@ -8,6 +7,5 @@ const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/users", createPostRoutes);
-router.use("/posts", postRoutes);
 
 export default router;
